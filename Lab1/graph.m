306fcca1698098728fa5d7c6eacff535a2fcb2ff
BrandenBeaman <brandenbeaman0@gmail.com>
2026-09-04 13:03:45 -0400

    Add files via upload
    added graph .m

---
+++ Lab1/graph.m
@@ -0,0 +1,13 @@
+init();
+
+x = [-15 -13 -11 -5 -2 -1 0 1 3]
+p = [1 15 0 -9]
+ysym = poly2sym(p)
+y = polyval(p,x)
+
+r = roots(p)
+
+make_plot(x, y, 'section 2b vecor with roots', 'x', 'y', r, [0 0 0], '*')
+
+
+
