1f10548287bfd27336a9f53ba321da2da462ea6a
BrandenBeaman <brandenbeaman0@gmail.com>
2026-09-09 16:54:51 -0400

    pre lab questions 1-2 done

---
+++ Lab2/prelab.m
@@ -0,0 +1,6 @@
+% 1. when x = 4, y evaulates to cos(x-4)/1 which equals 1/1 or 1
+%    when x = 2, y evaulates to -2/5
+
+% 2. x = (x1, x2, N) linspace makes a vector from x1 to x2 with N points
+% between
+x1 = linspace(1,10,5)
