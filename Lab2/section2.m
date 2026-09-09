1faaaf88bf151693096f71efb4b57b77b5566610
BrandenBeaman <brandenbeaman0@gmail.com>
2026-09-09 19:17:34 -0400

    completed section 2 and included file for 4) graph

---
+++ Lab2/section2.m
@@ -0,0 +1,16 @@
+%section2.m file for section 2 of lab 2
+init();
+
+%vector x with 1751 values between 0 and 10 
+x = linspace(0, 10, 1751);
+
+% Calculate the corresponding y values using y = ((x^2) - 16)/ (x - 4)
+% need .^ and ./ for element wise operation because x is a vector
+
+num = [1 0 -16];
+ynum = polyval (num, x);
+den = [1 -4];
+yden = polyval (den, x);
+y = ynum ./ yden;
+
+make_plot(x, y, "(x^2-16)/(x-4)", "x", "y")
