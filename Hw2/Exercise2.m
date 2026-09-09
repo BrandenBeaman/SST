d67dd0117d2f4ef7a96b6a112ee53841b968e44e
BrandenBeaman <brandenbeaman0@gmail.com>
2026-09-09 16:09:24 -0400

    HW2 exercise 2 working

---
+++ Hw2/Exercise2.m
@@ -0,0 +1,18 @@
+% (a)
+x1 = linspace(0,9,5)
+
+% (b)
+x2 = 0:5:15
+
+% (c)
+x3 = [3 4 5] + [1 2 4]
+
+% (d) produces an error in MATLAB (invalid use of operator)
+% To demonstrate, uncommenting the next line will error:
+% x4 = [3 4 5].+[1 2 4]
+
+% (e) element-wise multiplication
+x5 = [3 4 5].*[1 2 4]
+
+% (f) scalar multiplication
+x6 = 7*[1 2]
