6c1f855627ec1ccc1a65687afa068e92d8f376ee
BrandenBeaman <brandenbeaman0@gmail.com>
2026-09-09 18:56:22 -0400

    finished section 1 and added graph for t_

--- Lab2/section1.m
+++ Lab2/section1.m
@@ -10,7 +10,23 @@ w = 0.5;
 phase = -60 * (pi/180);
 
 theta = w*t + phase;
-
 y = (exp(1j * theta) +  exp(1j * theta))/ 2;
-
 make_plot(t, y, "cosine wave with 60 deg shift", "t", "y")
+
+% t is a column vector 
+size(t);
+
+%new vector named t_ with inracemnts = 4*pi/200 points not 201 because 0
+%counts as 1 - 201 = 200 steps
+t_inc = (4*pi/200);
+t_ = (0 : t_inc : 4*pi);
+
+%difference between t and t_ should be basically 0
+sum(abs(t-t_));
+
+%plot for t_
+make_plot(t_, y, "cosine wave with 60 deg shift", "t_", "y")
+
+
+
+
